eac13634c2dba25e16a31c094ca524bce9720089
Hung Vu <vunamhung@gmail.com>
2020-07-15 14:17:07 +0700

    add alias

--- craco.config.js
+++ craco.config.js
@@ -25,6 +25,8 @@ module.exports = {
         alias: {
             images: resolve(__dirname, "./src/assets/images"),
             scss: resolve(__dirname, "./src/assets/scss"),
+            components: resolve(__dirname, "./src/components"),
+            views: resolve(__dirname, "./src/views"),
         },
     },
 };
