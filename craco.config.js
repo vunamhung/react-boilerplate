124cd8f17eb40307f0b368807257e4f67d175547
Hung Vu <vunamhung@gmail.com>
2020-07-16 16:40:42 +0700

    :recycle: big refactor tailwind

--- craco.config.js
+++ craco.config.js
@@ -25,6 +25,7 @@ module.exports = {
         alias: {
             images: resolve(__dirname, "./src/assets/images"),
             scss: resolve(__dirname, "./src/assets/scss"),
+            css: resolve(__dirname, "./src/assets/css"),
             components: resolve(__dirname, "./src/components"),
             views: resolve(__dirname, "./src/views"),
             utilities: resolve(__dirname, "./src/utilities"),
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import logo from "images/logo.svg";
 import "scss/style.scss";
+import "css/tailwind.output.css";
 
 class App extends Component {
     render() {
         return (
             <div className="text-center">
-                <header className="bg-gray-800 min-h-screen flex items-center justify-center text-white flex-col">
+                <header className="bg-neutral-800 min-h-screen flex items-center justify-center text-white flex-col">
                     <img src={logo} className="logo" alt="logo" />
+                    <button className="button ~primary !high">dafsdfas</button>
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
