f2147605828b2e7f19f559dc7703f119de7e9710
Hung Vu <vunamhung@gmail.com>
2021-04-02 21:49:05 +0700

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   purge: {
     mode: 'layers',
     layers: ['utilities'],
-    content: ['src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', 'src/**/*.{js,ts,jsx,tsx}'],
   },
   theme: {
     extend: {
