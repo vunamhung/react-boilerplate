c07725c1b9b437d02066e8a02dd5a3693e33507c
Hung Vu <vunamhung@gmail.com>
2022-04-03 13:33:05 +0700

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
+const { spacing } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
-  darkMode: false,
-  purge: {
-    mode: 'layers',
-    layers: ['utilities'],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        neutral: colors.slate,
+        positive: colors.green,
+        urge: colors.violet,
+        warning: colors.yellow,
+        info: colors.blue,
+        critical: colors.red,
+      },
       container: {
         center: true,
-        padding: '2rem',
+        padding: spacing[6],
         screens: {
           sm: '100%',
           md: '640px',
@@ -18,16 +24,55 @@ module.exports = {
           xl: '1220px',
         },
       },
-      flex: {
-        2: '2 1 0%',
-        3: '3 1 0%',
+      screens: {
+        dlg: { max: '1023px' },
+        dmd: { max: '767px' },
+      },
+      maxWidth: {
+        'min-content': 'min-content',
+        'max-content': 'max-content',
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '3/4': '75%',
+        '1/5': '20%',
+        '2/5': '40%',
+        '3/5': '60%',
+        '4/5': '80%',
+      },
+      maxHeight: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '3/4': '75%',
+        '1/5': '20%',
+        '2/5': '40%',
+        '3/5': '60%',
+        '4/5': '80%',
+      },
+      minWidth: {
+        'min-content': 'min-content',
+        'max-content': 'max-content',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        '4/5': '80%',
+      },
+      minHeight: {
+        'min-content': 'min-content',
+        'max-content': 'max-content',
+        '1/4': '25vh',
+        '1/2': '50vh',
+        '3/4': '75vh',
+        '4/5': '80vh',
+      },
+      inset: {
+        '1/2': '50%',
+        '-1/2': '-50%',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-    require('tailwindcss-pseudo-selectors'),
-  ],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')({ strategy: 'class' })],
 };
