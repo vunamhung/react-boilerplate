97f1f8168ac53dc002cad3dd68732f25682722c7
Hung Vu <vunamhung@gmail.com>
2023-10-10 10:23:45 +0700

    🔥 refactor(root.tsx): remove unused fetcher import to improve code cleanliness and reduce bundle size 🔧 chore(root.tsx): remove fetcher from SWRConfig value to remove unnecessary dependency and improve performance

--- app/root.tsx
+++ app/root.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '~/App';
-import { fetcher } from '~/hooks/fetch';
 import Home from '~/routes/Home';
 import { SWRConfig } from 'swr';
 import '~/assets/css/style.css';
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
-    <SWRConfig value={{ fetcher, revalidateOnReconnect: true, errorRetryCount: 2 }}>
+    <SWRConfig value={{ revalidateOnReconnect: true, errorRetryCount: 2 }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
